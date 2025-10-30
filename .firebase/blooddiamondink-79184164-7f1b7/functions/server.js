4005705ab5cffe3516b8acf7a3e41bdac798a391
sebmazza1991 <sebmazza1991@gmail.com>
2025-10-30 16:38:17 +0000

    fix: Add .gitignore and remove .firebase folder from tracking

--- .firebase/blooddiamondink-79184164-7f1b7/functions/server.js
+++
@@ -1,4 +0,0 @@
-const { onRequest } = require('firebase-functions/v2/https');
-  const server = import('firebase-frameworks');
-  exports.ssrblooddiamondink79184 = onRequest({"id":"my-web-app","region":"europe-west4"}, (req, res) => server.then(it => it.handle(req, res)));
-  
