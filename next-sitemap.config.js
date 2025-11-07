164bd068e6245e61954698f3d206eeb7a5c2a0f0
Riven91 <Elementum_Publishing@proton.me>
2025-11-07 12:48:27 +0100

    add Google Search Console verification file

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,35 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://blooddiamond-tattoo.de',
-  generateRobotsTxt: true,
+  generateRobotsTxt: false,
   outDir: './public',
-  sitemapSize: 5000,
+  appDir: true,
+  generateIndexSitemap: true,
+  sitemapSize: 7000,
   changefreq: 'weekly',
-  priority: 0.8,
+  priority: 0.7,
+  exclude: ['/404', '/500', '/_not-found', '/error-preview', '/manifest.webmanifest'],
+  additionalPaths: async () => {
+    const routes = [
+      '/',
+      '/agb',
+      '/agb/boeblingen',
+      '/boeblingen',
+      '/datenschutz',
+      '/datenschutz/boeblingen',
+      '/franchise',
+      '/heilbronn',
+      '/impressum',
+      '/impressum/boeblingen',
+      '/pforzheim',
+      '/standorte',
+    ];
+    const now = new Date().toISOString();
+    return routes.map((loc) => ({
+      loc,
+      changefreq: 'weekly',
+      priority: loc === '/' ? 1.0 : 0.7,
+      lastmod: now,
+    }));
+  },
 };
